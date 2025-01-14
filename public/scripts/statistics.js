0159eb5f780428057997f4676a3abc05ca32cd50
Man <manctsui@gmail.com>
2025-01-13 17:09:31 -0800

    Fixed z- and t-test

--- public/scripts/statistics.js
+++ public/scripts/statistics.js
@@ -1,74 +1,74 @@
-function ztest() {
+function ztestanswer() {
     // Get inputs
     const popmean = Number(document.getElementById('popmean').value);
-    const popstd = Number(document.getElementById('popstd').value); 
-    const samplemean = Number(document.getElementById('samplemean').value);
-    const count = Number(document.getElementById('count').value);
-    // Do computationsa
-    const zscore = (samplemean - popmean)*Math.sqrt(count)/popstd;
-    const qvalue = simpson(0, Math.abs(zscore), 50, normalnumerator, 1/Math.sqrt(2*Math.PI));
-    // Write answer
-    const answer = document.getElementById('answer');
-    // answer.innerHTML = '\(e^x\) $$a^x$$';
-    answer.innerText = '\\(z\\)-score: ' + trunc(zscore) + 
-        '\nOne-sided \\(p\\)-value: ' + trunc(0.5 - qvalue) +
-        '\nTwo-sided \\(p\\)-value: ' + trunc(1 - 2* qvalue);
-    // KaTeX.RenderMath with default delimiter \(...\). Need escape \ first for JS. Can use either innerText or inner HTML since this is processed at the text level.
-    renderMathInElement(answer);
-    console.log('This is compute function at 3:31 PM.');
-}
-
-function ttest() {
-    // Get inputs
-    const popmean = Number(document.getElementById('popmean').value);
-    const samplestd = Number(document.getElementById('samplestd').value); 
+    const std = Number(document.getElementById('std').value); 
     const samplemean = Number(document.getElementById('samplemean').value);
     const count = Number(document.getElementById('count').value);
     // Do computations
-    const tscore = (samplemean - popmean)*Math.sqrt(count-1)/samplestd;
-    const qvalue = simpson(0, Math.abs(tscore), 50, normalnumerator, 1);
+    const z = (samplemean - popmean)*Math.sqrt(count)/std;
+    const p = ztest(Math.abs(z), count);
     // Write answer
     const answer = document.getElementById('answer');
-    // answer.innerHTML = '\(e^x\) $$a^x$$';
-    answer.innerText = '\\(z\\)-score: ' + trunc(tscore) + 
-        '\nOne-sided \\(p\\)-value: ' + trunc(0.5 - qvalue) +
-        '\nTwo-sided \\(p\\)-value: ' + trunc(1 - 2* qvalue);
+    answer.innerHTML = '<p>\\(z\\)-score: \\(' + z.toFixed(4) + 
+        '\\)</p><p>One-sided \\(p\\)-value: ' + p[0] +
+        '</p><p>Two-sided \\(p\\)-value: ' + p[1] + '</p>';
     // KaTeX.RenderMath with default delimiter \(...\). Need escape \ first for JS. Can use either innerText or inner HTML since this is processed at the text level.
     renderMathInElement(answer);
-    console.log('This is compute function at 3:31 PM.');
 }
 
-const words = [['So', 'Hence', 'Thus', 'Therefore','As a result'], ['Ba', 'Sa']];
-
-function trunc(x) {
-    return Math.trunc(10000*x)/10000.0;
+function ttestanswer() {
+    // Get inputs
+    const popmean = Number(document.getElementById('popmean').value);
+    const std = Number(document.getElementById('std').value); 
+    const samplemean = Number(document.getElementById('samplemean').value);
+    const count = Number(document.getElementById('count').value);
+    // Do computations
+    const t = (samplemean - popmean)*Math.sqrt(count)/std;
+    const p = ttest(Math.abs(t), count);
+    // Write answer
+    const answer = document.getElementById('answer');
+    answer.innerHTML = '<p>\\(t\\)-statistic: \\(' + t.toFixed(4) +
+        '\\)</p><p>Degrees of freedom: \\(' + (count-1) +
+        '\\)</p><p>One-sided \\(p\\)-value: '+ p[0] +
+        '</p><p>Two-sided \\(p\\)-value: '+ p[1] +'</p>';
+    // KaTeX.RenderMath with default delimiter \(...\). Need escape \ first for JS. Can use either innerText or inner HTML since this is processed at the text level.
+    renderMathInElement(answer);
 }
 
-function normalnumerator(x){
-    return Math.exp(-0.5*x**2);
-    // More efficient to divide after Simpson's rule, but I won't do it.
-    // Plug normalnumerator(), normaldenominator() into Simpson's rule.
+// Inputs: z = absolute value of zscore, partition = 500
+// If z < 5, integrate normal PDF from 0 to z with Simpson's rule with 500 partitions; use 5 digits
+// Else use approximation in Borjesson Table 1 Row 2 Subrow 4; use 4 digits
+function ztest(z) {
+    const p = (z < 5)? 
+        (0.5 - simpson(0, Math.abs(z), 500, x => Math.exp(-0.5*x**2)) * 1/Math.sqrt(2*Math.PI)) : 
+        1/(((1-0.271)*z + 0.271*Math.sqrt(z**2+7.356))*(Math.exp(0.5*(z**2)))*(Math.sqrt(2*Math.PI)));
+    return [trunc(p), trunc(2*p)];
 }
 
-
-// Simpson's rule assumes n = even
-// f = function
-// a, b = limits of integration
-// n = 
-// c = a multiplicative constant
-function simpson(a, b, n, f, c) {
-    const delta = (b-a)/n;
-    let point = 0;
-    // Simpson's rule at endpoints
-    let accum = f(a) + f(b);
-    // Simpson's rule at interior points
-    for (let i=1; i<n; i++) {
-        point += delta;
-        if (i%2 == 1) {
-            accum += 4*f(a+point);
-        } else if (i%2 == 0) {
-            accum += 2*f(a+point);
-        }
+// t-test. Estimate the integral in Geometric Approach book via Simpson's rule with partition=500.
+function ttest(t, n) {
+    let c = (n%2 == 1)? 1 : (2/Math.PI);
+    for (let i=n-2; i > 1; i=i-2) {
+        c = c*(i/(i-1));
     }
-    return accum*delta*c/3;
+    p = c*simpson(0, Math.atan2(Math.sqrt(n-1), t), 500, x => Math.sin(x)**(n-2));
+    return [trunc(p/2), trunc(p)];
+}
+
+function trunc(p) {
+    const e = Math.floor(Math.log10(p));
+    return (e>=-4)? ('\\(' + p.toPrecision(5) + '\\)') : ('\\(' + (p*10**(-1*e)).toFixed(3) + '×10^{'+e+'}\\)');
+}
+
+// Simpson's rule to estimate \int_a^b f(x)\;dx using n sample points
+function simpson(a, b, n, f) {
+    const delta = (b-a)/n;
+    let sum = f(b);
+    // Simpson's rule at interior points
+    for (let i=1, point=b; i<n; i++) {
+        point = point - delta;
+        sum += (i%2 == 1)? 4*f(point) : 2*f(point);
+    }
+    sum += f(a);
+    return sum*delta/3;
 }
